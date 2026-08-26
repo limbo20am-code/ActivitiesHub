70a70ea7a21a2ae853921dbe0e25f4326153fddc
limbo20am-code <limbo20am@gmail.com>
2026-08-25 22:08:24 -0600

    feat(parcial01) - add entity and EF biolerplate

---
+++ src/Domain/Activity.cs
@@ -0,0 +1,15 @@
+namespace Domain;
+
+public class Activity
+{
+    public string Id { get; set; } = Guid.NewGuid().ToString();
+    public required string Title { get; set; }
+    public DateTime Date { get; set; }
+    public required string Description { get; set; }
+    public required string Category { get; set; }
+    public bool IsCanceller { get; set; }
+    public required string City { get; set; }
+    public required string Venue { get; set; }
+    public required string Latitude { get; set; }
+    public required string Longitude { get; set; }
+}
