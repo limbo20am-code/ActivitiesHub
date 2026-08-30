7b734b806fdf913d3e6d443a4971fa46606ae3e6
limbo20am-code <limbo20am@gmail.com>
2026-08-29 22:15:01 -0600

    feat(parcial01) - add db initializer

--- src/API/Program.cs
+++ src/API/Program.cs
@@ -15,6 +15,21 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment()) { }
 
+using var scope = app.Services.CreateScope();
+var services = scope.ServiceProvider;
+
+try
+{
+    var context = services.GetRequiredService<AppDbContext>();
+    await context.Database.MigrateAsync();
+    await DbInitializer.SeedDataAsync(context);
+}
+catch (Exception ex)
+{
+    var logger = services.GetRequiredService<ILogger<Program>>();
+    logger.LogError(ex, "An error ocurred during database migration.");
+}
+
 
 app.MapControllers();
 
