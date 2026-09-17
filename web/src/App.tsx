76f2d43e8f84effbf1c908c52af1e408dc92b014
limbo20am-code <limbo20am@gmail.com>
2026-09-17 15:58:47 -0600

    feat(parcial02) - add activity type

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,19 +1,21 @@
-import {useEffect, useState } from 'react'
+import { useEffect, useState } from "react"
 
 function App() {
-  const [activities, setActivities] = useState([]);
+  const [activities, setActivities] = useState<Activity[]>([]);
 
   useEffect(() => {
     fetch('https://localhost:5001/api/v1/events')
-    .then(response => response.json())
-    .then(data => setActivities(data));
+      .then(response => response.json())
+      .then(data => setActivities(data));
+
+    return () => {};
   }, []);
 
-  return(
+  return (
     <div>
-      <h3 class="app" style={{ color: 'red' }}>Events Hub</h3>
+      <h3 style={{ color: 'red' }}>Events Hub</h3>
       <ul>
-        {activities.map((activity) => (
+        {activities.map((activity: Activity) => (
           <li key={activity.id}>{activity.title}</li>
         ))}
       </ul>
---
+++ web/src/lib/types/index.d.ts
@@ -0,0 +1,12 @@
+type Activity = {
+    id: string;
+    title: string;
+    date: string;
+    description: string;
+    category: string;
+    isCanceller: boolean;
+    city: string;
+    venue: string;
+    latitude: number;
+    longitude: number;  
+}
