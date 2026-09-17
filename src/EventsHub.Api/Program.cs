bafe61e271aa56bc8df283f0e5158e8759079575
limbo20am-code <limbo20am@gmail.com>
2026-09-17 09:55:52 -0600

    feat(parcial02) - add CORS

--- src/EventsHub.Api/Program.cs
+++ src/EventsHub.Api/Program.cs
@@ -9,10 +9,16 @@ builder.Services.AddDbContext<AppDbContext>(opt =>
 {
     opt.UseSqlite(builder.Configuration.GetConnectionString("SqliteConnection"));
 });
+builder.Services.AddCors(); 
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+app.UseCors(opt => opt
+    .AllowAnyHeader()
+    .AllowAnyMethod()
+    .WithOrigins("http://localhost:3000", "https://localhost:3000"));
+
 if (app.Environment.IsDevelopment()) { }
 
 using var scope = app.Services.CreateScope();
--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,6 +1,23 @@
+import {useEffect, useState } from 'react'
+
 function App() {
-  return (
-    <h3>Events Hub</h3>
+  const [activities, setActivities] = useState([]);
+
+  useEffect(() => {
+    fetch('https://localhost:5001/api/v1/events')
+    .then(response => response.json())
+    .then(data => setActivities(data));
+  }, []);
+
+  return(
+    <div>
+      <h3 class="app" style={{ color: 'red' }}>Events Hub</h3>
+      <ul>
+        {activities.map((activity) => (
+          <li key={activity.id}>{activity.title}</li>
+        ))}
+      </ul>
+    </div>
   )
 }
 
